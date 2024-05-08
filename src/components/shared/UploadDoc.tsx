4e01c53800816d37720e391cef9c45f0f76c267e
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-05-08 13:29:02 +0500

    need payment

--- src/components/shared/UploadDoc.tsx
+++ src/components/shared/UploadDoc.tsx
@@ -1,46 +1,82 @@
 "use client";
-
 import { useState } from "react";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 const UploadDoc = () => {
-  const [document, setDocument] = useState<Blob | undefined | null>(null);
+  const [document, setDocument] = useState<File | null | undefined>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const router = useRouter();
 
-  const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!document) {
+      setError("Please upload the document first");
+      return;
+    }
+
     setIsLoading(true);
-    console.log(document);
+    const formData = new FormData();
+    formData.append("pdf", document as Blob);
+    try {
+      const res = await fetch("/api/quizz/generate", {
+        method: "POST",
+        body: formData,
+      });
+      if (res.status === 200) {
+        const data = await res.json();
+        const quizzId = data.quizzId;
+
+        router.push(`/quizz/${quizzId}`);
+      }
+    } catch (e) {
+      console.log("error while generating", e);
+    }
+    setIsLoading(false);
+  };
+
+  const handleDocumentUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDocument(e?.target?.files?.[0]);
+    if (error) {
+      setError("");
+    }
   };
 
   return (
     <div className="w-full">
-      <form
-        className="w-full"
-        onSubmit={handleSubmit}
-      >
-        <label
-          className="bg-secondary w-full flex h-20 rounded-md border-4 border-dashed border-blue-900 relative"
-          htmlFor="document"
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <form
+          className="w-full"
+          onSubmit={handleSubmit}
         >
-          <div className="absolute inset-0 m-auto flex justify-center items-center">
-            {document && document?.name ? document.name : "Drag a file"}
-          </div>
-          <input
-            className="relative block w-full h-full z-50 opacity-0"
-            type="file"
-            id="document"
-            onChange={(evt) => setDocument(evt.target.files?.[0])}
-          />
-        </label>
-        <Button
-          className="mt-2"
-          size="lg"
-          type="submit"
-        >
-          Generate Quiz ✨
-        </Button>
-      </form>
+          <label
+            htmlFor="document"
+            className="bg-secondary w-full flex h-20 rounded-md border-4 border-dashed border-blue-900 relative"
+          >
+            <div className="absolute inset-0 m-auto flex justify-center items-center">
+              {document && document?.name ? document.name : "Drag a file"}
+            </div>
+            <input
+              type="file"
+              id="document"
+              className="relative block w-full h-full z-50 opacity-0"
+              onChange={handleDocumentUpload}
+            />
+          </label>
+          <p className="text-secondary-foreground my-2">Supported file types: pdf</p>
+          {error ? <p className="text-red-600">{error}</p> : null}
+          <Button
+            size="lg"
+            className="mt-2"
+            type="submit"
+          >
+            Generate Quizz 🪄
+          </Button>
+        </form>
+      )}
     </div>
   );
 };
